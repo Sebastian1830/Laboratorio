59894341c9c5686d57cc213bd6fc04532b1ca9a5
Sebastian Soto <rsotov1@upao.edu.pe>
2019-04-06 21:44:32 -0500

    segunda parte

--- app/src/main/java/com/example/laboratorio/MainActivity.java
+++ app/src/main/java/com/example/laboratorio/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.laboratorio;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
 
